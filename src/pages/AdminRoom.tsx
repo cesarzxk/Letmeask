0f9678d326ee26a1762858caad947a0039a0a736
César Vargas Dos Santos <43748428+cesarzxk@users.noreply.github.com>
2021-07-02 16:26:56 -0300

    The function delete question and delete room was implemented
    The handleEndRoom function is responsible for ending the room by the admin
    
    The handleDeleteQuestion function is responsible for removing the question by the admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,15 @@
 
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 import logoImg from '../assets/images/logo.svg';
+import deletImg from '../assets/images/delete.svg';
 import Button from '../components/button';
 import RoomCode from '../components/RoomCode';
 import Question from '../components/Question';
 
 import '../styles/room.scss';
 import useRoom from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
 
 
 type RoomParamsProps ={
@@ -17,6 +20,22 @@ export default function AdminRoom(){
     //const {user} = useContext(AuthContext);
     const params =useParams<RoomParamsProps>()
     const {questions, title} = useRoom(params.id)
+    const history = useHistory();
+
+    async function handleEndRoom(){
+        await database.ref(`rooms/${params.id}`).update({
+            endedAt: new Date(),
+        })
+
+        history.push('/')
+
+    }
+
+    async function handleDeleteQuestion(questionId:string){
+        if(window.confirm('Tem certeza que você deseja excluir essa pergunta?')){
+            await database.ref(`rooms/${params.id}/questions/${questionId}`).remove()
+        }
+    }
 
     
     return(
@@ -26,7 +45,7 @@ export default function AdminRoom(){
                     <img src={logoImg} alt="Letmeask"/>
                     <div>
                         <RoomCode code={params.id}/>
-                        <Button isOutlined >Encerrar sala</Button>
+                        <Button onClick={()=>handleEndRoom()} isOutlined >Encerrar sala</Button>
                     </div>
                 </div>
             </header>
@@ -45,7 +64,16 @@ export default function AdminRoom(){
                                 <Question 
                                 key={question.id}
                                 auth={question.author} 
-                                content={question.content}/>
+                                content={question.content}>
+                                    
+                                    <button
+                                        type='button'
+                                        onClick={()=>{handleDeleteQuestion(question.id)}}
+                                    >
+                                        <img src={deletImg} alt="Deletar pergunta." />
+                                    </button>
+
+                                </Question>
                             )
                         })
                     }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -35,7 +35,12 @@ export default function Home(){
         const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
         if(!roomRef.exists()){
-            alert('Room does not exists.')
+            alert('Room does not exists.');
+            return;
+        }
+
+        if(roomRef.val().endedAt){
+            alert('Essa sala foi encerrada!');
             return;
         }
 
